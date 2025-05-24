54e6b3353d3b6fa496854ee3ab5e56318307bc2e
piqueda <157534137+piqueda@users.noreply.github.com>
2025-05-24 15:59:33 -0300

    Mudanças
    Padronização utilizando arquivo.py e mudanças no programa principal

--- shellsort.py
+++ shellsort.py
@@ -1,13 +1,9 @@
 import sys
 import time
-
-def read_file(name):
-    with open(name, 'r') as file:
-        line = file.readline()
-    numbers = [int(num) for num in line.split()]
-    return numbers
+from arquivo import abrir, gravar
 
 def shell_sort(arr):
+    start = time.time()
     n = len(arr)
     gap = n // 2 
     while gap > 0:
@@ -20,25 +16,11 @@ def shell_sort(arr):
             arr[j] = temp
         gap //= 2
 
+    end=time.time()
+    gravar(sys.argv[2],(end-start)*1000)
+    print('Lista Ordenada:\n',arr)
     return arr
 
-if __name__ == "__main__":
-    name = "ordem_aleatoria.txt"
-    try:
-        arr = read_file(name)
-        print("Original Array: ")
-        print(arr)
-        start = time.perf_counter()
-        shell_sort(arr)
-        end = time.perf_counter()
-
-        execution_time = end - start
-        print("Sorted Array:")
-        print(arr)
-        print("Time Lapse: ")
-        print(f"{execution_time:.6f}")
-    except FileNotFoundError:
-        print("Error: File Not Found")
-    except ValueError:
-        print("Error: Invalid Value Found")
+numeros=abrir(sys.argv[1])
+shell_sort(numeros)
 
